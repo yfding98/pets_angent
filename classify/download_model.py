ccb70c0e61b18beb2de9b7619e3b9a7d2a04047b
dyf <codingdingyufan@163.com>
2025-06-16 21:17:37 +0800

    添加图片预分类接口

--- classify/download_model.py
+++
@@ -1,6 +0,0 @@
-import timm
-import torch
-
-# 下载并保存模型权重
-model = timm.create_model('mobilenetv3_small_100.lamb_in1k', pretrained=True)
-torch.save(model.state_dict(), 'mobilenetv3_small_100.lamb_in1k.pth')
